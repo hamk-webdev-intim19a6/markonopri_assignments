7ddf52f9a428dd93d3b892063632a4e66f219965
markonopri <marko.nopri@student.hamk.fi>
2020-09-11 19:39:06 +0300

    assignmentTwo version 1.0

---
+++ assignmentTwo.py
@@ -0,0 +1,24 @@
+import re
+import statistics
+
+# Getting user input
+print("###This program will give you sum, mean and median ###\n")
+
+userInput = input("Input a string: \n")
+
+# Funtion for sum
+def find_sum(str1): 
+    return sum(map(int,re.findall('\d+',userInput))) 
+
+# Funtion for mean
+def find_mean(str1): 
+    return statistics.mean(map(int,re.findall('\d+',userInput))) 
+
+# Funtion for median
+def find_median(str1): 
+    return statistics.median(map(int,re.findall('\d+',userInput))) 
+  
+# Printing all data for user
+print("Sum: ", find_sum(userInput)) 
+print("Mean: ", find_mean(userInput))
+print("Median: ", find_median(userInput))123
